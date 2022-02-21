4163c40952daec06f9f91bcbbcd086c90fd5da0b
geek_msato <msato@geek.co.jp>
2022-02-21 17:08:58 +0900

    コーディング

--- wp/wp-content/themes/deliv_2203/footer.php
+++ wp/wp-content/themes/deliv_2203/footer.php
@@ -1,4 +1,18 @@
-		<?php wp_footer(); ?>
+		<footer class="st-Footer">
+			<div class="st-Footer_Logo">
+				<a href="<?php echo home_url(); ?>">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/common/images/logo.png" alt="deliv2203">
+				</a>
+			</div>
+			<div class="st-Footer_Nav">
+				<ul class="st-Footer_List">
+					<li><a href="<?php echo home_url(); ?>/news/">NEWS</a></li>
+					<li><a href="<?php echo home_url(); ?>/recruit/">RECRUIT</a></li>
+				</ul>
+			</div>
+			<p class="st-Copy">&copy; 2022 Mikaze Sato.</p>
+		</footer>
 
+	<?php wp_footer(); ?>
 	</body>
 </html>
--- wp/wp-content/themes/deliv_2203/header.php
+++ wp/wp-content/themes/deliv_2203/header.php
@@ -1,9 +1,47 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
-		<link rel="profile" href="https://gmpg.org/xfn/11">
-		<?php wp_head(); ?>
+	<?php if (is_front_page()) : ?>
+	<title>トップ | 2203成果物 msato</title>
+	<?php else: ?>
+  <title><?php wp_title( ' | ','true','right' ); ?>2203成果物 msato</title>
+	<?php endif; ?>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
+	<meta name="keywords" content="成果物,2022年3月成果物,採用サイト">
+	<meta name="format-detection" content="telephone=no">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/common/css/common.css">
+	<?php if (is_front_page()): ?>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/top/css/top.css">
+	<?php endif; ?>
+	<?php if (is_front_page()): ?>
+	<meta name="description" content="2022年3月成果物。採用サイトを作成しました。">
+	<?php endif; ?>
+	<?php wp_head(); ?>
 	</head>
-	<body>
+
+	<?php if(is_user_logged_in()): ?>
+	<body id="body" class="wp-login">
+	<?php else: ?>
+	<body id="body">
+	<?php endif; ?>
+		<header class="st-Header">
+			<div class="st-Header_Logo">
+				<a href="<?php echo home_url(); ?>">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/common/images/logo.png" alt="deliv2203">
+				</a>
+			</div>
+			<div class="st-Header_Btn">
+				<span class="first"></span>
+				<span class="middle"></span>
+				<span class="last"></span>
+			</div>
+			<div class="st-Header_Nav">
+				<ul class="st-Header_List">
+					<li><a href="<?php echo home_url(); ?>/news/">NEWS</a></li>
+					<li><a href="<?php echo home_url(); ?>/recruit/">RECRUIT</a></li>
+					<li class="st-Header_Entry"><a href="<?php echo home_url(); ?>/entry/">ENTRY</a></li>
+				</ul>
+			</div>
+		</header>
