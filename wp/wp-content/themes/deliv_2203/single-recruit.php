a72da7bf68e6452032d5582791335351b5492f74
geek_msato <msato@geek.co.jp>
2022-03-26 23:21:36 +0900

    採用情報 関連する記事項目追加

--- wp/wp-content/themes/deliv_2203/single-recruit.php
+++ wp/wp-content/themes/deliv_2203/single-recruit.php
@@ -43,6 +43,57 @@ $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
       </div>
       <?php endif; ?>
 
+      <h3>関連する採用情報</h3>
+      <div class="recruit-index">
+        <?php // 現在表示されている投稿と同じタームに分類された投稿を取得
+          $taxonomy_slug = 'cat_recruit'; // タクソノミーのスラッグを指定
+          $post_type_slug = 'recruit'; // 投稿タイプのスラッグを指定
+          $post_terms = wp_get_object_terms($post->ID, $taxonomy_slug); // タクソノミーの指定
+          if( $post_terms && !is_wp_error($post_terms)) { // 値があるときに作動
+            $terms_slug = array(); // 配列のセット
+            foreach( $post_terms as $value ){ // 配列の作成
+              $terms_slug[] = $value->slug; // タームのスラッグを配列に追加
+            }
+          }
+          $args = array(
+            'post_type' => $post_type_slug, // 投稿タイプを指定
+            'posts_per_page' => 3, // 表示件数を指定
+            'post__not_in' => array($post->ID), // 現在の投稿を除外
+            'tax_query' => array( // タクソノミーパラメーターを使用
+              array(
+                'taxonomy' => $taxonomy_slug, // タームを取得タクソノミーを指定
+                'field' => 'slug', // スラッグに一致するタームを返す
+                'terms' => $terms_slug // タームの配列を指定
+              )
+            )
+          );
+          $the_query = new WP_Query($args); if($the_query->have_posts()):
+        ?>
+        <?php while ($the_query->have_posts()): $the_query->the_post(); ?>
+        <a href="<?php echo get_permalink(); ?>">
+          <div class="recruit-indexImg">
+            <?php if (get_field('recruit_mv')): ?>
+            <img src="<?php echo get_field('recruit_mv'); ?>" alt="" loading="lazy">
+            <?php else: ?>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/recruit/images/img_recruit.jpg" alt="" loading="lazy">
+            <?php endif; ?>
+          </div>
+          <div class="recruit-indexContent">
+            <h3><?php echo get_the_title(); ?></h3>
+            <?php
+              $cat_terms = get_the_terms( $post->ID, 'cat_recruit' );
+              if ($cat_terms && ! is_wp_error($cat_terms)):
+            ?>
+            <?php foreach($cat_terms as $cat_term): ?>
+            <span class="category"><?php echo $cat_term->name; ?></span>
+            <?php endforeach; endif; ?>
+          </div>
+        </a>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+        <?php endif; ?>
+      </div>
+
     </div>
   </div>
 </main>
