f60ea5e8d7f5cf183d617a052c7695f70a66fd87
geek_msato <msato@geek.co.jp>
2022-03-28 18:29:56 +0900

    js記述修正

--- front/src/script/_modules/scroll_top.js
+++ front/src/script/_modules/scroll_top.js
@@ -1,38 +1,36 @@
 console.log('scroll_top.js');
 
-window.onload = function() {
-  var pageTop = document.getElementById('js-scroll');
-  
-  pageTop.addEventListener('click', scroll_top);
-  
-  function scroll_top() {
-    window.scroll({
-      top: 0,
-      behavior: 'smooth',
-    })
-  };
-  
-  window.addEventListener('scroll', scroll_event);
-  
-  function scroll_event() {
-    if(window.pageYOffset > 300) {
-      pageTop.classList.add('show');
-    } else {
-      pageTop.classList.remove('show');
-    }
-  };
-  
-  window.addEventListener('scroll', scroll_stop);
-  
-  function scroll_stop() {
-    var scroll = window.pageYOffset;
-    var height = window.innerHeight;
-    var headerHeight = document.getElementById('footer').clientHeight;
-    var footerPosition = document.getElementById('footer').offsetTop + headerHeight - 40;
-    if(scroll + height >= footerPosition) {
-      pageTop.classList.add('absolute');
-    } else {
-      pageTop.classList.remove('absolute');
-    }
-  };
-}
+var pageTop = document.getElementById('js-scroll');
+
+pageTop.addEventListener('click', scroll_top);
+
+function scroll_top() {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth',
+  })
+};
+
+window.addEventListener('scroll', scroll_event);
+
+function scroll_event() {
+  if(window.pageYOffset > 300) {
+    pageTop.classList.add('show');
+  } else {
+    pageTop.classList.remove('show');
+  }
+};
+
+window.addEventListener('scroll', scroll_stop);
+
+function scroll_stop() {
+  var scroll = window.pageYOffset;
+  var height = window.innerHeight;
+  var headerHeight = document.getElementById('footer').clientHeight;
+  var footerPosition = document.getElementById('footer').offsetTop + headerHeight - 40;
+  if(scroll + height >= footerPosition) {
+    pageTop.classList.add('absolute');
+  } else {
+    pageTop.classList.remove('absolute');
+  }
+};
--- front/src/script/_modules/sp_nav.js
+++ front/src/script/_modules/sp_nav.js
@@ -1,10 +1,8 @@
 console.log('sp_nav.js');
 
-window.onload = function() {
-  var nav = document.getElementById('js-spNav');
-  var hamburger = document.getElementById('js-hamburger');
+var nav = document.getElementById('js-spNav');
+var hamburger = document.getElementById('js-hamburger');
 
-  hamburger.addEventListener('click', function() {
-    nav.classList.toggle('open');
-  })
-}
+hamburger.addEventListener('click', function() {
+  nav.classList.toggle('open');
+})
